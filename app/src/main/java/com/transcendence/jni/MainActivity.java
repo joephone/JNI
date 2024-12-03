6bde7a6f0f192aa83b0d3893933fa00a86c50af3
joephone <270136049@qq.com>
2024-12-03 23:25:07 +0800

    init

---
+++ app/src/main/java/com/transcendence/jni/MainActivity.java
@@ -0,0 +1,37 @@
+package com.transcendence.jni;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.transcendence.jni.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'jni' library on application startup.
+    static {
+        System.loadLibrary("jni");
+    }
+
+    /**
+     * A native method that is implemented by the 'jni' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+}
