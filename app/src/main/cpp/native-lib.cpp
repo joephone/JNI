8bb0ef463db99b41fabc852c90f5a28634b51b93
joephone <270136049@qq.com>
2024-12-13 00:55:51 +0800

    java call  jni  method

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -10,8 +10,16 @@ Java_com_transcendence_jni_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_transcendence_jni_MainActivity_changeName(JNIEnv *env, jobject thiz) {
+extern "C"  //代表下面的代码采用C的编译方式
+JNIEXPORT
+void  //代表Java中的void
+JNICALL  //也是一个关键字，可以少， 约束函数入栈顺序，和堆栈内存清理的规则
+Java_com_transcendence_jni_MainActivity_changeName(JNIEnv *env, jobject thiz) {  //1代表java的下划线
+    //如果当前是  c   (*env)->xxx函数   如果是CPP env-> xxx函数   因为如果是C  *env是二级指针  JNINativeInterface*是一级指针 又加了个*所以成为二级指针  别名+* 所以CPP是一级指针
+    jclass mainActivityCls = env->FindClass("com/transcendence/jni/MainActivity");
 
+    jfieldID  nameFid = env->GetFieldID(mainActivityCls,"name","Ljava/lang/String;");
+
+    jstring  value = env->NewStringUTF("修改为joe");
+    env->SetObjectField(thiz,nameFid,value);
 }
--- app/src/main/java/com/transcendence/jni/MainActivity.java
+++ app/src/main/java/com/transcendence/jni/MainActivity.java
@@ -3,6 +3,7 @@ package com.transcendence.jni;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.transcendence.jni.databinding.ActivityMainBinding;
@@ -14,8 +15,9 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("jni");
     }
 
+    public native String stringFromJNI();
 
-    public native void changeName();   //alt + enter 创建
+    public native void changeName();   //alt + enter 创建   改变我们的属性 name
 
     private String name = "joephone";    //调用C++ 修改为 joe
 
@@ -31,11 +33,17 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = binding.sampleText;
         tv.setText(stringFromJNI());
+
+        Button btnShow = binding.btnShow;
+        btnShow.setOnClickListener(v -> {
+            System.out.println("修改前的name:"+name);
+            changeName();
+            System.out.println("修改后的name:"+name);
+            btnShow.setText("修改后的name:"+name);
+        });
+
     }
 
-    /**
-     * A native method that is implemented by the 'jni' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+
+
 }
